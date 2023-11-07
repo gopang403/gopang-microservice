78fec9e5ef1c0c728db95bb917520d9154e8a59a
jhcode33 <jhcode33@gmail.com>
2023-11-07 12:14:05 +0900

    Chore: Add Dependency spring boot web, jpa, mariaDB, lombok
    - 프로젝트에 필요한 의존성을 추가했습니다

---
+++ itemserver/src/main/java/org/gopang/ItemServerApplication.java
@@ -0,0 +1,9 @@
+package org.gopang;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServerApplication {
+    public static void main(String[] args) {
+    }
+}
